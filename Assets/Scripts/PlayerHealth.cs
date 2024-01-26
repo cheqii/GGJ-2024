f29dcb5146e22577ba18213126b398976e6d9c68
loneguss <pitcha.huta@bumail.net>
2024-01-27 01:46:06 +0700

    add attack area

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Handle enemy death, e.g., play death animation, destroy GameObject, etc.
+        Destroy(gameObject);
+    }
+}
